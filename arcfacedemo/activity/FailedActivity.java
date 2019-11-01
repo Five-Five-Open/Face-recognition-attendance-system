ff8ab970e41a85c61121372d0efd73160c2630d9
qiuqian <54809269+tanyueqiuqian@users.noreply.github.com>
2019-11-01 07:21:45 -0500

    Add files via upload

---
+++ arcfacedemo/activity/FailedActivity.java
@@ -0,0 +1,15 @@
+package com.arcsoft.arcfacedemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arcsoft.arcfacedemo.R;
+
+public class FailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_failed);
+    }
+}
