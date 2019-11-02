c4c8e69172558ed9120831af8294387b2da5ca7a
qiuqian <54809269+tanyueqiuqian@users.noreply.github.com>
2019-11-02 06:27:56 -0500

    Add files via upload

---
+++ arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "G6srFdQzGac4HLmDfaMrDHL8bh75N4cEr74RsyZRizKh";
+    public static final String SDK_KEY = "vwfgnEB2tzXzqSZoocy2YRayffTXEArCtWmMwMAq2ub";
+}
